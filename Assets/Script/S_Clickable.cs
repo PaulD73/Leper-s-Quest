2a0ec1878dda16fe66858639b9845787545bdd53
Khr0n0ss <jenfantbis@laposte.net>
2025-10-20 13:31:52 +0200

    Initial Commit

---
+++ Assets/Script/S_Clickable.cs
@@ -0,0 +1,24 @@
+
+using UnityEngine;
+public class MouseInputExample : MonoBehaviour
+{
+
+    public GameObject self;
+    void Update()
+    {
+        
+    }
+
+    //Agrandie lorsque souris dessus
+    void OnMouseEnter()
+    {
+        self.transform.localScale = self.transform.localScale * 1.1f;
+    }
+
+    //Rétrécie lorsque souris dessus
+    void OnMouseExit()
+    {
+        self.transform.localScale = self.transform.localScale / 1.1f;
+    }
+
+}
