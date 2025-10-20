bc068540f4562724fe6ad20f874b173961bbf23f
Khr0n0ss <jenfantbis@laposte.net>
2025-10-20 13:50:58 +0200

    Statistique

---
+++ Assets/Script/S_Statistique.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+[System.Serializable]
+public struct Statistique
+{
+    public string name;
+    public float degat;
+    public float pv;
+    public int necrose;
+}
+
+
+public class S_Statistique : MonoBehaviour
+{
+    public Statistique stat;
+}
